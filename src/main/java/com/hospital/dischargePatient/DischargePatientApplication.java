cf122157d2671bfd666706b3009b92ba76522410
Varun Reddy Musku <varunreddymusku@gmail.com>
2024-06-01 11:12:45 -0500

    intial commits

---
+++ src/main/java/com/hospital/dischargePatient/DischargePatientApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.dischargePatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DischargePatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DischargePatientApplication.class, args);
+	}
+
+}
